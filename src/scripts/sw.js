eb97b823afd506ff20a04396009bfda19e281279
Saepudin <saepudinjeh@gmail.com>
2021-05-27 18:27:20 +0700

    add workbox caching

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,6 +1,38 @@
+import "regenerator-runtime";
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
+const { assets } = global.serviceWorkerOption;
+console.log(assets);
+
 if (workbox)
-  console.log(`Workbox berhasil dimuat`);
-else
-  console.log(`Workbox gagal dimuat`);
+    console.log(`Workbox berhasil dimuat`);
+else {
+
+    console.log(`Workbox gagal dimuat`);
+}
+
+workbox.routing.registerRoute(
+    new RegExp("https://restaurant-api.dicoding.dev/"),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: "Api",
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 10
+            })
+        ]
+    })
+);
+
+workbox.routing.registerRoute(
+    new RegExp("/"),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: "pages",
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 10
+            })
+        ]
+    })
+);
+
+
